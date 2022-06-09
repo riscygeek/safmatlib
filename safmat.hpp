7a15f6ff7fe095e4f14cbbed7bd57184783b76a4
Benjamin Stürz <benni@stuerz.xyz>
2022-06-09 19:57:23 +0200

    Fixed copyright boilerplate
    Signed-off-by: Benjamin Stürz <benni@stuerz.xyz>

--- safmat.hpp
+++ safmat.hpp
@@ -2,7 +2,7 @@
     Copyright (C) 2022 Benjamin Stürz <benni@stuerz.xyz>
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lessser General Public License as published
+    it under the terms of the GNU Lesser General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
